9a297202be33aaeebea82e86c66bd58d0a142f70
Lukas <38840142+lukasstorck@users.noreply.github.com>
2025-10-15 20:06:32 +0200

    feat: add Material Symbols Outlined and bootstrap

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import DataContextProvider from "./context/DataContextProvider";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <DataContextProvider>
