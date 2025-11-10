25066b7ac5df62b89209270d23cc978586213538
Lukas <38840142+lukasstorck@users.noreply.github.com>
2025-11-10 11:33:16 +0100

    chore: use correct types

--- src/web/src/context/DataContext.ts
+++ src/web/src/context/DataContext.ts
@@ -3,49 +3,49 @@ import { createContext } from "react";
 import type { Device, CustomKeybind, SlotPresets, User } from "../types";
 
 export interface DataContextType {
-  customKeybinds: CustomKeybind[];
+  readonly customKeybinds: CustomKeybind[];
   setCustomKeybinds: React.Dispatch<React.SetStateAction<CustomKeybind[]>>;
 
-  slotPresets: SlotPresets;
-  setSlotPresets: (newSlotPresets: SlotPresets) => void;
+  readonly slotPresets: SlotPresets;
+  setSlotPresets: React.Dispatch<React.SetStateAction<SlotPresets>>;
 
-  userColor: string;
+  readonly userColor: string;
   setUserColor: React.Dispatch<React.SetStateAction<string>>;
 
-  userName: string;
+  readonly userName: string;
   setUserName: React.Dispatch<React.SetStateAction<string>>;
 
-  connectionStatus: number;
-  devicesById: Record<string, Device>;
-  devicesBySlot: Record<number, Device>;
+  readonly connectionStatus: number;
+  readonly devicesById: Record<string, Device>;
+  readonly devicesBySlot: Record<number, Device>;
 
-  groupId: string;
+  readonly groupId: string;
   setGroupId: React.Dispatch<React.SetStateAction<string>>;
 
-  groupState: { users: User[]; devices: Device[] };
+  readonly groupState: { users: User[]; devices: Device[] };
 
   handleJoinGroup: (groupId: string) => void;
-  handleLeaveGroup: React.Dispatch<void>; // TODO: fix typing
+  handleLeaveGroup: () => void;
   handleRenameOutput: (deviceId: string, newName: string) => void;
   handleSelectKeybindPreset: (deviceSlot: number, presetName: string) => void;
   handleSelectOutput: (deviceId: string, state: boolean) => void;
 
-  user: User | null;
+  readonly user: User | null;
 
-  userId: string | null;
+  readonly userId: string | null;
   setUserId: React.Dispatch<React.SetStateAction<string | null>>;
 
-  activeKeybinds: Record<string, [string, string][]>;
+  readonly activeKeybinds: Record<string, [string, string][]>;
 
-  customKeybindActiveListener: number | null;
+  readonly customKeybindActiveListener: number | null;
   setCustomKeybindActiveListener: React.Dispatch<
     React.SetStateAction<number | null>
   >;
 
-  showKeybindEditor: boolean;
-  setShowKeybindEditor: (newState: boolean) => void;
+  readonly showKeybindEditor: boolean;
+  setShowKeybindEditor: React.Dispatch<React.SetStateAction<boolean>>;
 
-  usersById: Record<string, User>;
+  readonly usersById: Record<string, User>;
 }
 
 export const DataContext = createContext<DataContextType | undefined>(
